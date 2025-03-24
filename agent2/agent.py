542891ab79064b7d066057f6fdfee1cff3fba265
Paul S. Schweigert <paul@paulschweigert.com>
2025-03-24 11:05:13 -0400

    adding crewai agent
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>

---
+++ agent2/agent.py
@@ -0,0 +1,48 @@
+import os
+
+from crewai import Crew, LLM, Agent, Task, Process
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+app = FastAPI()
+
+@app.post("/")
+async def main():
+    # Define your custom LLM configuration
+    my_llm = LLM(
+        base_url = f"{os.getenv('BASE_URL')}/v1",
+        model = "openai/meta-llama/llama-3-1-70b-instruct",
+        api_key = os.getenv("API_KEY"),
+        extra_headers = {"RITS_API_KEY": os.getenv("API_KEY")}
+    )
+
+    # Create a researcher agent
+    researcher = Agent(
+      role='Senior Researcher',
+      goal='Discover groundbreaking technologies',
+      verbose=True,
+      llm=my_llm,
+      backstory='A curious mind fascinated by cutting-edge innovation and the potential to change the world, you know everything about tech.'
+    )
+
+    # Task for the researcher
+    research_task = Task(
+        description = os.getenv("PROMPT"),
+        expected_output = 'A summary of the results',
+        agent = researcher
+    )
+
+    # Instantiate your crew
+    tech_crew = Crew(
+      agents=[researcher],
+      tasks=[research_task],
+      process=Process.sequential
+    )
+
+    # Begin the task execution
+    crewoutput = tech_crew.kickoff()
+    return crewoutput.raw
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="warning")
